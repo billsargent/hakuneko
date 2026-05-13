76f865b7f6592802c225157b9685b9087dacf543
billsargent <bill@station51.net>
2026-05-13 13:00:45 +0200

    Revert "Move import queue & settings storage to local storage/ folder with SQLite"
    This reverts commit 67e389b5ef901d7172e57b09ee65b20822a9bb35.

--- src/web/mjs/engine/Settings.mjs
+++ src/web/mjs/engine/Settings.mjs
@@ -80,10 +80,10 @@ export default class Settings extends EventTarget {
             label: 'Bookmarks Directory',
             description: [
                 'The directory where the bookmark and chaptermark files will be stored.',
-                'Data now lives in the local storage/ folder within the app directory.'
+                'This setting has no effect when the application is in portable mode!'
             ].join('\n'),
-            input: types.disabled,
-            value: app.getAppPath() + '/storage'
+            input: process.env.HAKUNEKO_PORTABLE ? types.disabled : types.directory,
+            value: app.getPath('userData')
         };
 
         this.useSubdirectory = {
